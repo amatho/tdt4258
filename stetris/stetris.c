86d457163a54c61c2db57dbbfae44abcf991df5c
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2021-11-13 00:20:00 +0100

    stetris: first working version

--- stetris/stetris.c
+++ stetris/stetris.c
@@ -1,9 +1,16 @@
+#define _DEFAULT_SOURCE
+
+#include <fcntl.h>
+#include <inttypes.h>
+#include <linux/fb.h>
 #include <linux/input.h>
 #include <poll.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <sys/select.h>
 #include <termios.h>
 #include <time.h>
@@ -15,10 +22,14 @@
 #define ROW_CLEAR (1 << 1)
 #define TILE_ADDED (1 << 2)
 
+#define FRAME_BUFFER_SIZE 64
+#define TILE_COLOR_TABLE_SIZE 7
+
 // If you extend this structure, either avoid pointers or adjust
 // the game logic allocate/deallocate and reset the memory
 typedef struct {
     bool occupied;
+    uint16_t color;
 } tile;
 
 typedef struct {
@@ -48,6 +59,20 @@ typedef struct {
                                 // lowers with increasing level, never reaches 0
 } gameConfig;
 
+typedef struct {
+    int32_t joy_fd;
+    int32_t fb_fd;
+    struct fb_fix_screeninfo fb_fix_info;
+    struct fb_var_screeninfo fb_var_info;
+    uint16_t *led_fb;
+} sense_hat_t;
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} pixel_rgb_t;
+
 gameConfig game = {
     .grid = {8, 8},
     .uSecTickTime = 10000,
@@ -55,26 +80,163 @@ gameConfig game = {
     .initNextGameTick = 50,
 };
 
+sense_hat_t SENSE_HAT;
+
+// A table of RGB565 values to use for the tiles
+uint16_t tile_color_table[TILE_COLOR_TABLE_SIZE] = {
+    0xF800, 0xFBE0, 0xFFE0, 0x7E0, 0x7FF, 0x1F, 0xF81F};
+// A wrapping index into the color table
+uint32_t tile_color_index = 0;
+
+int32_t findJoystick() {
+    int32_t fd = -1;
+    int32_t i = 0;
+
+    while (i < 32) {
+        char path[19];
+        sprintf(path, "/dev/input/event%d", i);
+        fd = open(path, O_RDONLY);
+
+        if (fd < 0) {
+            return fd;
+        }
+
+        char name[32];
+        if (ioctl(fd, EVIOCGNAME(sizeof(name)), name) >= 0) {
+            if (strncmp(name, "Raspberry Pi Sense HAT Joystick", 31) == 0) {
+                return fd;
+            }
+        }
+
+        close(fd);
+        i++;
+    }
+
+    return -1;
+}
+
+int32_t findFrameBuffer() {
+    int32_t fd = -1;
+    int32_t i = 0;
+
+    while (i < 32) {
+        char path[10];
+        sprintf(path, "/dev/fb%d", i);
+        fd = open(path, O_RDWR);
+
+        if (fd < 0) {
+            return fd;
+        }
+
+        struct fb_fix_screeninfo info;
+        if (ioctl(fd, FBIOGET_FSCREENINFO, &info) >= 0) {
+            if (strncmp(info.id, "RPi-Sense FB", 12) == 0) {
+                return fd;
+            }
+        }
+
+        close(fd);
+        i++;
+    }
+
+    return -1;
+}
+
 // This function is called on the start of your application
 // Here you can initialize what ever you need for your task
 // return false if something fails, else true
-bool initializeSenseHat() { return true; }
+bool initializeSenseHat() {
+    int32_t joy_fd = findJoystick();
+    if (joy_fd < 0) {
+        fprintf(stderr, "could not find the joystick\n");
+        return false;
+    }
+
+    SENSE_HAT.joy_fd = joy_fd;
+
+    int32_t fb_fd = findFrameBuffer();
+    if (fb_fd < 0) {
+        fprintf(stderr, "could not find the LED frame buffer\n");
+        return false;
+    }
+
+    SENSE_HAT.fb_fd = fb_fd;
+
+    if (ioctl(SENSE_HAT.fb_fd, FBIOGET_FSCREENINFO, &SENSE_HAT.fb_fix_info) <
+        0) {
+        fprintf(stderr, "could not get frame buffer fixed screen info\n");
+        return false;
+    }
+
+    if (ioctl(SENSE_HAT.fb_fd, FBIOGET_VSCREENINFO, &SENSE_HAT.fb_var_info) <
+        0) {
+        fprintf(stderr, "could not get frame buffer variable screen info\n");
+        return false;
+    }
+
+    if (SENSE_HAT.fb_var_info.bits_per_pixel != 16) {
+        fprintf(stderr, "frame buffer has invalid bits per pixel\n");
+        return false;
+    }
+
+    SENSE_HAT.led_fb =
+        mmap(0, SENSE_HAT.fb_fix_info.smem_len, PROT_READ | PROT_WRITE,
+             MAP_SHARED, SENSE_HAT.fb_fd, 0);
+
+    return true;
+}
 
 // This function is called when the application exits
 // Here you can free up everything that you might have opened/allocated
-void freeSenseHat() {}
+void freeSenseHat() {
+    munmap(SENSE_HAT.led_fb, SENSE_HAT.fb_fix_info.smem_len);
+    close(SENSE_HAT.joy_fd);
+    close(SENSE_HAT.fb_fd);
+}
 
 // This function should return the key that corresponds to the joystick press
 // KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, with the respective direction
 // and KEY_ENTER, when the the joystick is pressed
 // !!! when nothing was pressed you MUST return 0 !!!
-int readSenseHatJoystick() { return 0; }
+int readSenseHatJoystick() {
+    struct pollfd fds = {.fd = SENSE_HAT.joy_fd, .events = POLLIN};
+    int32_t ev_len = poll(&fds, 1, 0);
+
+    int32_t key = 0;
+    if (ev_len < 0) {
+        fprintf(stderr, "joystick poll returned an error");
+    } else if (ev_len == 0) {
+        return key;
+    }
+
+    struct input_event events[ev_len];
+    read(SENSE_HAT.joy_fd, events,
+         sizeof(struct input_event) * (uint32_t)ev_len);
+
+    for (int32_t i = 0; i < ev_len; i++) {
+        struct input_event ev = events[i];
+        if (ev.type == EV_KEY && ev.value == 1) {
+            key = ev.code;
+        }
+    }
+
+    return key;
+}
 
 // This function should render the gamefield on the LED matrix. It is called
 // every game tick. The parameter playfieldChanged signals whether the game
 // logic has changed the playfield
 void renderSenseHatMatrix(bool const playfieldChanged) {
-    (void)playfieldChanged;
+    if (!playfieldChanged) {
+        return;
+    }
+
+    for (uint32_t j = 0; j < game.grid.y; j++) {
+        for (uint32_t i = 0; i < game.grid.x; i++) {
+            SENSE_HAT.led_fb[(j * game.grid.y) + i] =
+                game.playfield[j][i].color;
+        }
+    }
 }
 
 // The game logic uses only the following functions to interact with the
@@ -83,6 +245,9 @@ void renderSenseHatMatrix(bool const playfieldChanged) {
 
 static inline void newTile(coord const target) {
     game.playfield[target.y][target.x].occupied = true;
+    game.playfield[target.y][target.x].color =
+        tile_color_table[tile_color_index];
+    tile_color_index = (tile_color_index + 1) % TILE_COLOR_TABLE_SIZE;
 }
 
 static inline void copyTile(coord const to, coord const from) {
@@ -183,18 +348,14 @@ bool clearRow() {
 
 void advanceLevel() {
     game.level++;
-    switch (game.nextGameTick) {
-    case 1:
-        break;
-    case 2 ... 10:
+
+    // Rewritten to fix errors about GNU case range extension when using clang
+    if (game.nextGameTick >= 2 && game.nextGameTick <= 10)
         game.nextGameTick--;
-        break;
-    case 11 ... 20:
+    else if (game.nextGameTick >= 11 && game.nextGameTick <= 20)
         game.nextGameTick -= 2;
-        break;
-    default:
+    else if (game.nextGameTick == 0 || game.nextGameTick > 20)
         game.nextGameTick -= 10;
-    }
 }
 
 void newGame() {
@@ -240,7 +401,7 @@ bool sTetris(int const key) {
         if (game.tick == 0) {
             // We communicate the row clear and tile add over the game state
             // clear these bits if they were set before
-            game.state &= ~(ROW_CLEAR | TILE_ADDED);
+            game.state &= (unsigned int)~(ROW_CLEAR | TILE_ADDED);
 
             playfieldChanged = true;
             // Clear row if possible
@@ -351,7 +512,7 @@ void renderConsole(bool const playfieldChanged) {
 }
 
 inline unsigned long uSecFromTimespec(struct timespec const ts) {
-    return ((ts.tv_sec * 1000000) + (ts.tv_nsec / 1000));
+    return (unsigned long)((ts.tv_sec * 1000000) + (ts.tv_nsec / 1000));
 }
 
 int main(int argc, char **argv) {
@@ -363,7 +524,7 @@ int main(int argc, char **argv) {
     {
         struct termios ttystate;
         tcgetattr(STDIN_FILENO, &ttystate);
-        ttystate.c_lflag &= ~(ICANON | ECHO);
+        ttystate.c_lflag &= (tcflag_t) ~(ICANON | ECHO);
         ttystate.c_cc[VMIN] = 1;
         tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
     }
@@ -412,8 +573,8 @@ int main(int argc, char **argv) {
         // Wait for next tick
         gettimeofday(&eTv, NULL);
         unsigned long const uSecProcessTime =
-            ((eTv.tv_sec * 1000000) + eTv.tv_usec) -
-            ((sTv.tv_sec * 1000000 + sTv.tv_usec));
+            (unsigned long)(((eTv.tv_sec * 1000000) + eTv.tv_usec) -
+                            ((sTv.tv_sec * 1000000 + sTv.tv_usec)));
         if (uSecProcessTime < game.uSecTickTime) {
             usleep(game.uSecTickTime - uSecProcessTime);
         }
